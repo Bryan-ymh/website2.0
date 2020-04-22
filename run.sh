cb1b52c16e43030569bafa70399643bb6b1d5f9d
timoyuen <timo@timoyuens-iMac.local>
2020-04-22 11:35:15 +0800

    run.sh to suit new folder path

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
- docker run -p 80:8080 -v /Users/timo/Downloads/hugo-ws/quickstart/public:/var/www/html -v /Users/timo/Downloads/dl-1/config/nginx.conf:/etc/nginx/nginx.conf trafex/alpine-nginx-php7
+ docker run -p 80:8080 -v /Users/timo/Downloads/hugo-ws/blogger/public:/var/www/html -v /Users/timo/Downloads/dl-1/config/nginx.conf:/etc/nginx/nginx.conf trafex/alpine-nginx-php7
